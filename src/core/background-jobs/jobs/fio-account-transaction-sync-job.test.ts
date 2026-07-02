042d186097e7a2949b3f012800acbf89a821eace
JanetByte <JanetByte@users.noreply.github.com>
2026-07-02 12:22:26 +0000

    test: wait for FIO sync pointer update

--- src/core/background-jobs/jobs/fio-account-transaction-sync-job.test.ts
+++ src/core/background-jobs/jobs/fio-account-transaction-sync-job.test.ts
@@ -159,14 +159,14 @@ describe("fio account transaction sync job", () => {
     expect(requestedUrls).toEqual([
       "https://fioapi.fio.cz/v1/rest/periods/fio-token-1/2026-03-31/2026-05-31/transactions.json",
     ])
-    expect(
-      await evolu.loadQuery(fioPluginSyncPointerQuery(fioPluginId))
-    ).toEqual([
-      {
-        id: fioPluginId,
-        lastSyncedDate: "2026-05-31",
-      },
-    ])
+    await expect
+      .poll(() => evolu.loadQuery(fioPluginSyncPointerQuery(fioPluginId)))
+      .toEqual([
+        {
+          id: fioPluginId,
+          lastSyncedDate: "2026-05-31",
+        },
+      ])
     expect(errors).toEqual([])
   })
 

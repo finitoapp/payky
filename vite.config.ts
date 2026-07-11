6f1c02a5928f52cac9ad7708d681a1c2c208fa61
richardmeszaros <hello@upweb.sk>
2026-07-11 22:50:07 +0200

    feat(capacitor): add iOS target

--- vite.config.ts
+++ vite.config.ts
@@ -133,6 +133,7 @@ export default (({ command }: ConfigEnv) => {
       ],
     },
     test: {
+      setupFiles: ["./src/test/setup.ts"],
       coverage: {
         provider: "v8",
         reporter: ["text", "html", "lcov"],
